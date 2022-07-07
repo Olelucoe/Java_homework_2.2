f9e8903caa154d5381cd85a24e5322e05e02228f
Oleg Titkin <o.a.t@inbox.ru>
2022-07-07 23:44:32 +0300

    changed Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
         int bonus;
         if (amount > 1000) {
             bonus = amount / 100;
-        } else bonus = 0;
-        int item_score = score + amount + bonus;
-        System.out.println("итоговый счёт:" + item_score);
+        } else {
+            bonus = 0;
+        } ;
+        int itemScore = score + amount + bonus;
+        System.out.println("итоговый счёт:" + itemScore);
         System.out.println("бонусных рублей:" + bonus);
     }
 }
